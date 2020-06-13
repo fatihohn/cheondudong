dc4323add99a95061e5de3b9ee9f108ac0d9bfa6
fatihohn <fatihohn@gmail.com>
2020-06-13 21:17:50 +0900

    head

---
+++ cdd_head.php
@@ -0,0 +1,17 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">	
+    
+    <title>천두동-동두천</title>
+
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="robots" content="noindex">
+
+	<meta property="og:url" content="https://천두동도메인.org">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="천두동-동두천">
+	<meta property="og:description" content="천두동의 장소들">
+	<meta property="og:image" content="대표 이미지 제작 후 입력">
+
+  	<link rel="apple-touch-icon" sizes="180x180" href="아이콘 제작 후 입력">
+    <link rel="icon" type="image/png" sizes="32x32" href="아이콘 제작 후 입력">
+    <link rel="icon" type="image/png" sizes="16x16" href="아이콘 제작 후 입력">
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-echo "hello world";
-?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
 
+<?php include "cdd_head.php"; ?>
+
+
+    </head>
+    <body>
+        
+    </body>
+</html>
