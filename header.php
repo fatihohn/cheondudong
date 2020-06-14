7811b09deb59329cee0a99b2c992687c6e53ea1e
fatihohn <fatihohn@gmail.com>
2020-06-14 23:02:32 +0900

    header new div

--- header.php
+++ header.php
@@ -1,25 +1,30 @@
 <div id="header_wrap">
     <div id="header_box">
-        <div id="header_title">
-            <a href="/index.php">
-            <h1>
-                천두동
-            </h1>
-        </a>
-        </div>
-        <div id="header_btn">
-            <a>
-            
-                <!-- <img src="/static/img/menu_btn.png" alt="메뉴"> -->
-                <div class="menu_btn">
-                    <h2 class="menu_o">
-                        ○
-                    </h2>
-                    <h2 class="menu_m">
-                        ≡
-                    </h2>
-                </div>
-            </a>
+        <div id="header_box_area">
+
+            <div id="header_title">
+                <a href="/index.php">
+                    <h1>
+                        천두동
+                    </h1>
+                </a>
+            </div>
+            <div id="header_btn">
+                <a>
+                    
+                    <!-- <img src="/static/img/menu_btn.png" alt="메뉴"> -->
+                    <div class="menu_btn">
+                        <h2 class="menu_o">
+                            ○
+                        </h2>
+                        <h2 class="menu_m">
+                            ≡
+                        </h2>
+                    </div>
+                </a>
+            </div>
+
+
         </div>
     </div>
 </div>
