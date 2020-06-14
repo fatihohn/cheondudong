e0ca0bbf0f9da8cb726d7814253cc1fe6021a85c
fatihohn <fatihohn@gmail.com>
2020-06-14 22:55:05 +0900

    head css link

--- head.php
+++ head.php
@@ -16,6 +16,6 @@
     <link rel="icon" type="image/png" sizes="32x32" href="아이콘 제작 후 입력">
     <link rel="icon" type="image/png" sizes="16x16" href="아이콘 제작 후 입력">
 
-    <link rel="stylesheet" href="/static/css/all.css?after" type="text/css">
-    <link rel="stylesheet" href="/static/css/header.css?after" type="text/css">
-    <link rel="stylesheet" href="/static/css/main.css?after" type="text/css">
+    <link rel="stylesheet" href="static/css/all.css?after" type="text/css">
+    <link rel="stylesheet" href="static/css/header.css?after" type="text/css">
+    <link rel="stylesheet" href="static/css/main.css?after" type="text/css">
