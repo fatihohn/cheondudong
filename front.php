d5e0099bc2f95db336793f90eb7b04695151dcb7
fatihohn <fatihohn@gmail.com>
2020-06-14 14:38:19 +0900

    front init

---
+++ front.php
@@ -0,0 +1,22 @@
+<div class="front_wrap">
+    <div class="front_box">
+        <div class="front_title">
+            <div class="title_cdd">
+                <!-- <p>
+                    천두동         
+                </p> -->
+                <p>천</p>
+                <p>두</p>
+                <p>동</p>
+            </div>
+            <div class="titld_ddc">
+                <!-- <p>
+                    동두천         
+                </p> -->
+                <p>동</p>
+                <p>두</p>
+                <p>천</p>
+            </div>
+        </div>
+    </div>
+</div>
