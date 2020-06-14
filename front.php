e469ad0c88f6c4940ac5ff49efaf81f0dae155d6
fatihohn <fatihohn@gmail.com>
2020-06-14 18:35:23 +0900

    front font

--- front.php
+++ front.php
@@ -1,9 +1,10 @@
 <div id="front_wrap">
     <div id="front_box">
         <div id="front_title">
-            <div id="title_cdd">
+            <div id="title_cdd gg-batang">
      
-                <h1 class="gg-batang">
+                <!-- <h1 class="gg-batang"> -->
+                <h1 >
                     천<br>
                     두<br>
                     동
@@ -11,7 +12,8 @@
             </div>
             <div id="title_ddc">
            
-                <h1 class="gg-batang">
+                <!-- <h1 class="gg-batang"> -->
+                <h1>
                     동<br>
                     두<br>
                     천
@@ -27,16 +29,18 @@
 
         let frontTitle = document.getElementById("front_title");
         function hideFront() {
-            let frontWrap = document.getElementById("front_wrap");
             let headerWrap = document.getElementById("header_wrap");
-            let mapWrap = document.getElementById("map_wrap");
+            let frontWrap = document.getElementById("front_wrap");
             let detailWrap = document.getElementById("detail_wrap");
+            let mapWrap = document.getElementById("map_wrap");
             let menuWrap = document.getElementById("menu_wrap");
-            frontWrap.style.display = "none";
+            let footerWrap = document.getElementById("footer_wrap");
             headerWrap.style.display = "initial";
-            mapWrap.style.display = "initial";
+            frontWrap.style.display = "none";
             detailWrap.style.display = "none";
+            mapWrap.style.display = "initial";
             menuWrap.style.display = "none";
+            footerWrap.style.display = "none";
         }
         frontTitle.addEventListener("click", hideFront);
     }
---
+++ header.php
@@ -0,0 +1,7 @@
+<div id="header_wrap">
+    <div id="header_box">
+        <div id="header_title">
+            
+        </div>
+    </div>
+</div>
