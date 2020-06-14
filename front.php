031a91ccda11bc7fa624fc7ecd9cdc34e4028b31
fatihohn <fatihohn@gmail.com>
2020-06-14 16:48:18 +0900

    front font

--- front.php
+++ front.php
@@ -2,30 +2,23 @@
     <div id="front_box">
         <div id="front_title">
             <div id="title_cdd">
-                <!-- <p>
-                    천두동         
-                </p> -->
-                
-                <!-- <p>천</p>
-                <p>두</p>
-                <p>동</p> -->
-                <h1 class="gg-batang">천<br>
-                   두<br>
-                   동</h1>
+     
+                <h1 class="gg-batang">
+                    천<br>
+                    두<br>
+                    동
+                </h1>
             </div>
             <div id="title_ddc">
-                <!-- <p>
-                    동두천         
-                </p> -->
-
-                <!-- <p>동</p>
-                <p>두</p>
-                <p>천</p> -->
-                <h1 class="gg-batang">동<br>
-                   두<br>
-                   천</h1>
+           
+                <h1 class="gg-batang">
+                    동<br>
+                    두<br>
+                    천
+                </h1>
 
             </div>
         </div>
     </div>
 </div>
+
