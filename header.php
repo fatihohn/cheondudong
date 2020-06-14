153b55ec9b07179334e3df567bf0724bfa579847
fatihohn <fatihohn@gmail.com>
2020-06-15 00:00:16 +0900

    header func

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <div id="header_box_area">
 
             <div id="header_title">
-                <a href="/index.php">
+                <a>
                     <h1 class="gg-batang">
                         천두동
                     </h1>
@@ -28,3 +28,27 @@
         </div>
     </div>
 </div>
+
+<script>
+function headerTitleClick() {
+
+let headerTitle = document.getElementById("header_title");
+function initMap() {
+    let headerWrap = document.getElementById("header_wrap");
+    let frontWrap = document.getElementById("front_wrap");
+    let detailWrap = document.getElementById("detail_wrap");
+    let mapWrap = document.getElementById("map_wrap");
+    let menuWrap = document.getElementById("menu_wrap");
+    let footerWrap = document.getElementById("footer_wrap");
+    headerWrap.style.display = "initial";
+    frontWrap.style.display = "none";
+    detailWrap.style.display = "none";
+    mapWrap.style.display = "initial";
+    menuWrap.style.display = "none";
+    footerWrap.style.display = "none";
+}
+headerTitle.addEventListener("click", initMap);
+}
+headerTitleClick();
+
+</script>
