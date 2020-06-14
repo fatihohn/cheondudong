369b84f9b7ce978b8990cfe712b050f5dc7dcf2b
fatihohn <fatihohn@gmail.com>
2020-06-14 19:09:30 +0900

    header init

--- header.php
+++ header.php
@@ -1,7 +1,14 @@
 <div id="header_wrap">
     <div id="header_box">
         <div id="header_title">
-            
+            <h1>
+                천두동
+            </h1>
+        </div>
+        <div id="header_btn">
+            <a>
+                <img src="/static/img/menu_btn.png" alt="메뉴">
+            </a>
         </div>
     </div>
 </div>
