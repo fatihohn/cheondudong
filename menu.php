11449e207e4ad052478c1b45d6c519fddb746af6
fatihohn <fatihohn@gmail.com>
2020-06-15 11:12:31 +0900

    menu layout

--- menu.php
+++ menu.php
@@ -3,19 +3,25 @@
         <div id="menu_box_area">
             <ul id="menu_list">
                 <li id="menu_intro" class="menu_list_item">
-                    <h2 class="gg-batang">
-                        천두동 소개
-                    </h2>
+                    <a >
+                        <h2 class="gg-batang">
+                            천두동 소개
+                        </h2>
+                    </a>
                 </li>
                 <li id="menu_place" class="menu_list_item">
-                    <h2 class="gg-batang">
-                        천두동 장소들
-                    </h2>
+                    <a>
+                        <h2 class="gg-batang">
+                            천두동 장소들
+                        </h2>
+                    </a>
                 </li>
                 <li id="menu_people" class="menu_list_item">
-                    <h2 class="gg-batang">
-                        천두동 사람들
-                    </h2>
+                    <a>
+                        <h2 class="gg-batang">
+                            천두동 사람들
+                        </h2>
+                    </a>
                 </li>
             </ul>
         </div>
