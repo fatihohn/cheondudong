78b71cb3fdd49f97b3bedadfdefa0f6659a0410a
fatihohn <fatihohn@gmail.com>
2020-06-16 13:17:58 +0900

    menu func

--- menu.php
+++ menu.php
@@ -3,36 +3,77 @@
         <div id="menu_box_area">
             <ul id="menu_list">
                 <li id="menu_intro" class="menu_list_item">
-                    <a >
+                    <a id="menu_intro_btn">
                         <h2 class="gg-batang">
                             천두동·동두천 소개
                         </h2>
                     </a>
                     <div id="intro_cont_wrap">
-
+                        hello intro
                     </div>
                 </li>
                 <li id="menu_place" class="menu_list_item">
-                    <a>
+                    <a id="menu_place_btn">
                         <h2 class="gg-batang">
                             천두동 장소들
                         </h2>
                     </a>
                     <div id="place_cont_wrap">
-
+                        hello place
                     </div>
                 </li>
                 <li id="menu_people" class="menu_list_item">
-                    <a>
+                    <a id="menu_people_btn">
                         <h2 class="gg-batang">
                             천두동 사람들
                         </h2>
                     </a>
                     <div id="people_cont_wrap">
-
+                        hello people
                     </div>
                 </li>
             </ul>
         </div>
     </div>
 </div>
+
+<script>
+ function menuIntroClick() {
+
+let menuIntro = document.getElementById("menu_intro_btn");
+function showIntro() {
+    let introWrap = document.getElementById("intro_cont_wrap");
+    
+    introWrap.style.display = "initial";
+    
+}
+menuIntro.addEventListener("click", showIntro);
+}
+menuIntroClick();
+
+ function menuPlaceClick() {
+
+let menuPlace = document.getElementById("menu_place_btn");
+function showPlace() {
+    let placeWrap = document.getElementById("place_cont_wrap");
+    
+    placeWrap.style.display = "initial";
+    
+}
+menuPlace.addEventListener("click", showPlace);
+}
+menuPlaceClick();
+
+ function menuPeopleClick() {
+
+let menuPeople = document.getElementById("menu_people_btn");
+function showPeople() {
+    let peopleWrap = document.getElementById("people_cont_wrap");
+    
+    peopleWrap.style.display = "initial";
+    
+}
+menuPeople.addEventListener("click", showPeople);
+}
+menuPeopleClick();
+</script>
