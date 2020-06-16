7df163450a04a4169a82442f1e6e175798ed9b6a
fatihohn <fatihohn@gmail.com>
2020-06-16 15:51:46 +0900

    map init

--- map.php
+++ map.php
@@ -7,7 +7,7 @@
     <script>
     mapboxgl.accessToken = 'pk.eyJ1Ijoic3VyaWNpdHkiLCJhIjoiY2tiZnpzaGtzMTB5NTJwcWVtOHF5anRmMCJ9.CI4QuMCsvVak3vrNtnJWcw';
     var map = new mapboxgl.Map({
-    container: '<your HTML element id>',
+    container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
