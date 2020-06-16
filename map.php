4a3d0363afe4cf266529957eb2e5a47b281a0f44
fatihohn <fatihohn@gmail.com>
2020-06-16 15:56:08 +0900

    map init

--- map.php
+++ map.php
@@ -8,7 +8,8 @@
     mapboxgl.accessToken = 'pk.eyJ1Ijoic3VyaWNpdHkiLCJhIjoiY2tiZnpzaGtzMTB5NTJwcWVtOHF5anRmMCJ9.CI4QuMCsvVak3vrNtnJWcw';
     var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    // style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    style: 'mapbox://styles/suricity/ckbg1kxyd3jjo1is4ro4nkpb5', // stylesheet location
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
     });
