24a5ebef2b9699b3a7084ef2c1a80fd14ad07f8e
fatihohn <fatihohn@gmail.com>
2020-06-16 17:55:10 +0900

    map init

--- map.php
+++ map.php
@@ -12,7 +12,7 @@
     style: 'mapbox://styles/suricity/ckbg1kxyd3jjo1is4ro4nkpb5', // stylesheet location
     // center: [192, 37], // starting position [lng, lat]
     center: [127.084960, 37.911627], // starting position [lng, lat]
-    zoom: 12.8 // starting zoom
+    zoom: 13.2 // starting zoom
     });
 
     // document.querySelector(".mapboxgl-canvas").style.width = "100%";
