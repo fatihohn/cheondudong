30ac5497b4c23fb7fbef56923a0622b3da206e22
fatihohn <fatihohn@gmail.com>
2020-06-16 22:55:33 +0900

    detail layout

--- detail.php
+++ detail.php
@@ -16,29 +16,29 @@ $placeRef = "임시 참고자료 == 리스트 == 입력 시 리스트로 변환"
     <div id="detail_box">
         <div id="detail_box_area">
             <div id="detail_marker">
-                <img src="<?php $placeMarker;?>" alt="<?php $placeTitle;?>">
+                <img src="<?php echo $placeMarker;?>" alt="<?php echo $placeTitle;?>">
             </div>
             <div id="detail_title">
                 <h3>
-                    <?php $placeTitle;?>
+                    <?php echo $placeTitle;?>
                 </h3>
             </div>
             <div id="detail_point">
                 <p class="detail_address">
-                    <?php $placeAddress;?>
+                    <?php echo $placeAddress;?>
                 </p>
                 <p class="detail_coord">
-                    <?php $placeCoord;?>
+                    <?php echo $placeCoord;?>
                 </p>
             </div>
             <div id="detail_cont">
-                <?php $placeCont;?>
+                <?php echo $placeCont;?>
             </div>
             <div id="detail_work">
-                <?php $placeWork;?>
+                <?php echo $placeWork;?>
             </div>
             <div id="detail_ref">
-                <?php $placeRef;?>
+                <?php echo $placeRef;?>
             </div>
         </div>
     </div>
