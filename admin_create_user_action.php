77acc28fc0cb5cd30fd9b7e24ca98692e0b0623d
fatihohn <fatihohn@gmail.com>
2020-06-17 21:53:27 +0900

    comment

--- admin_create_user_action.php
+++ admin_create_user_action.php
@@ -31,19 +31,6 @@ $password = base64_encode(hash('sha512', $pwSalt, true));
 $email = $_POST['email'];
 $email = mysqli_real_escape_string($conn, $email);
 
-// $author = $_POST['author'];
-// // $author = mysqli_real_escape_string($conn, $author);
-// // $author = mysql_real_escape_string($author);
-
-// $auth_detail = $_POST['auth_detail'];
-// // $auth_detail = mysqli_real_escape_string($conn, $auth_detail);
-
-// $cast = $_POST['cast'];
-// $cast = mysqli_real_escape_string($conn, $cast);
-
-// $active = $_POST['active'];
-// $active = mysqli_real_escape_string($conn, $active);
-
 $pw_one = $_POST['password'];
 $pw_one = mysqli_real_escape_string($conn, $pw_one);
 
@@ -102,12 +89,11 @@ $nameCheck = mysqli_query($conn, $nameSql);
                     mysqli_stmt_bind_param($stmt, "sssss", $realname, $username, $password, $salt, $email);
                     // mysqli_stmt_execute($stmt);
                     // $result = mysqli_stmt_get_result($stmt);
-                    if(!mysqli_stmt_execute($stmt)){
+                    if(!mysqli_stmt_execute($stmt)) {
                     // if($result === false){
                         echo '저장실패. 관리자에게 문의해주세요';
                         error_log(mysqli_error($conn));
-                    }
-                    else{
+                    } else {
                         echo("<script>alert('회원가입이 완료되었습니다.');location.href='admin_index.php';</script>");
                     }
                     // mysqli_stmt_close();
