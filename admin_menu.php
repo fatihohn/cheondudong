ff5ac3e427e0a9da6ea4a2528278de672ec52090
fatihohn <fatihohn@gmail.com>
2020-06-18 01:58:51 +0900

    menu add func

--- admin_menu.php
+++ admin_menu.php
@@ -11,8 +11,8 @@ $placeTitle = "임시 장소 제목"
         <div id="menu_box_area">
             <ul id="menu_list">
                 <li id="menu_intro" class="menu_list_item">
-                        <h2 class="gg-batang tooling">
-                    <a id="menu_intro_btn">
+                        <h2 class="gg-batang">
+                    <a id="menu_intro_btn" class="tooling">
                             천두동·동두천 소개
                     </a>
                     <a id="add_intro" class="tooling">
