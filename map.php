756d6128ce2fcc17fced1dc19b65fcd9ddd2ae7b
fatihohn <fatihohn@gmail.com>
2020-06-18 10:44:56 +0900

    map layout

--- map.php
+++ map.php
@@ -13,8 +13,35 @@
     style: 'mapbox://styles/suricity/ckbhx3huo0xb11ip5hywb59rx', // stylesheet location
     // center: [192, 37], // starting position [lng, lat]
     center: [127.060444, 37.911627], // starting position [lng, lat]
-    zoom: 12.9 // starting zoom
+    maxBounds: [
+	    	// 	// strictly seongbukgu
+	    	// 	// [126.972368, 37.572532], // Southwest coordinates
+	    	// 	// [127.073682, 37.629226]  // Northeast coordinates
+	    		
+	    	// 	// limit hapjeong <-> nowon 
+	    	// 	[126.907793, 37.547946], // Southwest limit coordinates 
+	    	// 	[127.093138, 37.643414]  // Northeast limit coordinates
+
+	    	// limit seoul city 
+	    	[127.061343, 37.843099], // Southwest limit coordinates 
+	    	[127.064452, 38.027590] // Northeast limit coordinates
+
+
+		    ],
+    zoom: 12.9, // starting zoom
+    maxZoom: 18
     });
+    var language = new MapboxLanguage({
+			  defaultLanguage: "ko"
+		});
+		map.addControl(language);
+
+		geocoder = new MapboxGeocoder({
+			mapboxgl: mapboxgl,
+		    accessToken: mapboxgl.accessToken,
+		});
+
+		map.addControl(geocoder);
 
     // document.querySelector(".mapboxgl-canvas").style.width = "100%";
     // document.querySelector(".mapboxgl-canvas").style.height = "100%";
