2f16c5939602068b392657584e2bb9aaa85480bc
fatihohn <fatihohn@gmail.com>
2020-06-18 10:56:18 +0900

    map js init

--- static/js/map.js
+++ static/js/map.js
@@ -1,10 +1,4 @@
-<div id="map_wrap">
-    <div id='map' style='width: 100%; height: 100%;'></div>
-</div>
 
-
-
-    <!-- <script>
     mapboxgl.accessToken = 'pk.eyJ1Ijoic3VyaWNpdHkiLCJhIjoiY2tiZnpzaGtzMTB5NTJwcWVtOHF5anRmMCJ9.CI4QuMCsvVak3vrNtnJWcw';
     var map = new mapboxgl.Map({
     container: 'map',
@@ -35,7 +29,3 @@
 
 		// map.addControl(geocoder);
 
-    
-    </script> -->
-
-    <script src="static/js/map.js"></script>
