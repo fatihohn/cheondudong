e76cb994546549ed8bf03eb507ee940d4927fe14
fatihohn <fatihohn@gmail.com>
2020-06-19 14:27:20 +0900

    map marker

---
+++ admin_header-SURFACE-OHNDONGHUN.php
@@ -0,0 +1,122 @@
+<div id="header_wrap">
+    <div id="header_box">
+        <div id="header_box_area">
+            <div id="header_login">
+            <a>
+                               
+                                <?php
+        include 'cdd_db_conn.php';
+        $query = "select * from user_data order by id desc";
+        $result = $conn->query($query);
+        $total = mysqli_num_rows($result);
+        
+        session_start();
+        
+        if (isset($_SESSION['username'])) {
+            ?> <div onclick="location.href='./admin_logout.php'"><h4 class="tooling">
+            <?php
+            echo $_SESSION['username'];
+                        
+            ?>님 [LogOut]</h4></div>
+            
+            <?php
+        } else {
+            ?> 
+            
+                <!-- <div onclick="location.href='./admin_login.php'"><h4 class="tooling">[LogIn]</h4></div>
+                <div onclick="location.href='./admin_create_user.php'"><h4 class="tooling">[SignIn]</h4></div> -->
+                <div id="login_btn"><h4 class="tooling">[LogIn]</h4></div>
+                <div id="signin_btn"><h4 class="tooling">[SignIn]</h4></div>
+<!-- <script>
+    function loginClick() {
+        let loginBtn = document.getElementById("login_btn");
+        function showLogin() {
+            location.href="admin_login.php";
+        }
+        loginBtn.addEventListener("click", showLogin);
+    }
+    loginClick();
+    
+    function signinClick() {
+        let signinBtn = document.getElementById("signin_btn");
+        function showsignin() {
+            location.href="admin_create_user.php";
+        }
+        signinBtn.addEventListener("click", showsignin);
+    }
+    signinClick();
+</script> -->
+    <?php   }
+    ?>  
+                            </a>
+            </div>
+            <div id="header_title">
+                <a>
+                    <h1 class="gg-batang">
+                        천두동·에디터
+                    </h1>
+                </a>
+            </div>
+            <div id="header_btn">
+                <a>
+                    
+                    <div id="menu_btn">
+                        <img src="/static/img/menu_btn.png" alt="메뉴">
+                        <!-- <h2 class="menu_o">
+                            ○
+                        </h2>
+                        <h2 class="menu_m">
+                            ≡
+                        </h2> -->
+                    </div>
+                </a>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+<!-- 
+<script>
+    function headerTitleClick() {
+
+        let headerTitle = document.getElementById("header_title");
+        function showMap() {
+            let headerWrap = document.getElementById("header_wrap");
+            let frontWrap = document.getElementById("front_wrap");
+            let detailWrap = document.getElementById("detail_wrap");
+            let mapWrap = document.getElementById("map_wrap");
+            let menuWrap = document.getElementById("menu_wrap");
+            let footerWrap = document.getElementById("footer_wrap");
+            headerWrap.style.display = "initial";
+            frontWrap.style.display = "none";
+            detailWrap.style.display = "none";
+            mapWrap.style.display = "initial";
+            menuWrap.style.display = "none";
+            footerWrap.style.display = "none";
+        }
+        headerTitle.addEventListener("click", showMap);
+    }
+    headerTitleClick();
+
+    function headerMenuClick() {
+
+        let headerMenu = document.getElementById("header_btn");
+        function showMenu() {
+            let headerWrap = document.getElementById("header_wrap");
+            let frontWrap = document.getElementById("front_wrap");
+            let detailWrap = document.getElementById("detail_wrap");
+            let mapWrap = document.getElementById("map_wrap");
+            let menuWrap = document.getElementById("menu_wrap");
+            let footerWrap = document.getElementById("footer_wrap");
+            headerWrap.style.display = "initial";
+            frontWrap.style.display = "none";
+            detailWrap.style.display = "none";
+            mapWrap.style.display = "none";
+            menuWrap.style.display = "initial";
+            footerWrap.style.display = "initial";
+        }
+        headerMenu.addEventListener("click", showMenu);
+    }
+    headerMenuClick();
+</script> -->
--- map.php
+++ map.php
@@ -38,4 +38,192 @@
     
     </script> -->
 
-    <script src="static/js/map.js"></script>
+
+
+    <!-- <script src="static/js/map.js"></script> -->
+
+    <script>
+    var map;
+	var location_data = {"country":"", "city":"", "sublocality_level_1" : "", "sublocality_level_2" : ""}
+
+	var geocoder;
+	var built_address = "";
+	var is_dongducheon = false;
+	var user_coordinates;
+	var geocode_results;
+	var distance_from_dongducheon;
+	var map_markers;
+	var floaters_markers;
+	var link_lines_features_work;
+
+	var supports_location = false;
+	var check_location_interval;
+	var location_interval_is_running = false;
+	var location_interval_is_first_check = true;
+	var is_checking_location = false;
+
+	var first_load = true;
+	var data_loaded = false;
+	var url_params;
+
+	var hiddenHeight;
+
+	var quotes_array = [];
+	var base64_export;
+	var detectrtc_tested = false;
+    
+    
+    
+    
+    
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic3VyaWNpdHkiLCJhIjoiY2tiZnpzaGtzMTB5NTJwcWVtOHF5anRmMCJ9.CI4QuMCsvVak3vrNtnJWcw';
+    
+    var geojson = {
+'type': 'FeatureCollection',
+'features': [
+{
+'type': 'Feature',
+'properties': {
+'message': 'Foo',
+'iconSize': [865, 320]
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [127.065629, 37.942684]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'message': 'Bar',
+'iconSize': [865, 320]
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [127.095351, 37.926185]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'message': 'Baz',
+'iconSize': [865, 320]
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [127.095351, 37.926185]
+}
+}
+]
+};
+    
+    
+    
+    var map = new mapboxgl.Map({
+    container: 'map',
+    // style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    // style: 'mapbox://styles/suricity/ckbg1kxyd3jjo1is4ro4nkpb5', // stylesheet location
+    style: 'mapbox://styles/suricity/ckbhx3huo0xb11ip5hywb59rx', // stylesheet location
+    // center: [192, 37], // starting position [lng, lat]
+    center: [127.060444, 37.911627], // starting position [lng, lat]
+    maxBounds: [
+	    	//limit dongducheon 
+	    	[126.954480, 37.846739], // Southwest limit coordinates 
+	    	[127.196154, 38.014146] // Northeast limit coordinates
+		    ],
+    zoom: 12.9, // starting zoom
+    maxZoom: 18
+    });
+
+    // add markers to map
+geojson.features.forEach(function(marker) {
+// create a DOM element for the marker
+var el = document.createElement('div');
+el.className = 'marker';
+el.style.backgroundImage =
+'url(http://http://13.209.210.87/static/img/marker/' +
+marker.properties.iconSize.join('/') +
+'/)';
+el.style.width = marker.properties.iconSize[0] + 'px';
+el.style.height = marker.properties.iconSize[1] + 'px';
+ 
+el.addEventListener('click', function() {
+window.alert(marker.properties.message);
+});
+ 
+// add marker to map
+new mapboxgl.Marker(el)
+.setLngLat(marker.geometry.coordinates)
+.addTo(map);
+});
+
+
+
+
+    var language = new MapboxLanguage({
+			  defaultLanguage: "ko"
+		});
+		map.addControl(language);
+
+		geocoder = new MapboxGeocoder({
+			mapboxgl: mapboxgl,
+		    accessToken: mapboxgl.accessToken,
+		});
+
+        map.addControl(geocoder);
+
+        
+        
+
+//****user location****//
+
+function getUserLocation(method) {
+
+    toggle_updatelocation(true);
+
+    // request to allow user position 
+    if (navigator.geolocation) {
+        supports_location = true;
+
+        toggle_updatelocation(false);
+
+        console.log("L1, getUserLocation()")
+        navigator.geolocation.getCurrentPosition(showPosition, show_location_error);
+
+
+        function showPosition(position) {
+
+            // get user current coordinates and center map on coordiates
+            console.log("L2", position)
+            //console.log(position.coords.latitude, position.coords.latitude)
+            user_coordinates = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            // convert users coordinates to place address
+            getReverseGeocode(user_coordinates, method)
+
+            distance_from_dongducheon = getDistanceFromLatLonInKm(user_coordinates.lat,user_coordinates.lng,37.916149,127.057049)
+
+        }
+    } else {
+        // if device doesnt support location
+        console.log("E1, device doesnt support location")
+        show_location_error(error)
+    }
+
+    // if device supports camera, show camera buttons
+    DetectRTC.load(function() {
+        if (DetectRTC.hasWebcam == false && detectrtc_tested != true) {
+            console.log("device has no cam" );
+            $(".btn_opencam").hide();
+            $(".ui_map_bottom").addClass("no_cam");
+            detectrtc_tested = true;
+        } 
+    });
+}; 
+
+
+
+    </script>
