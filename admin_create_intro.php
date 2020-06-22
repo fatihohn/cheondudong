fe3382342398890e5def1d4c188fbf60907c8c5c
fatihohn <fatihohn@gmail.com>
2020-06-23 00:14:28 +0900

    create intro action

--- admin_create_intro.php
+++ admin_create_intro.php
@@ -22,94 +22,79 @@
                             <h3>천두동 소개</h3>
                         </center>
         
-<?php    
-include 'cdd_db_conn.php';   
-$URL = "./admin_index.php";
+                        <?php    
+                        include 'cdd_db_conn.php';   
+                        $URL = "./admin_index.php";
 
-$uname = $_SESSION['username'];
-$query = "SELECT * FROM user_data WHERE username= '$uname'";
-$result = $conn->query($query);
-$rows = mysqli_fetch_assoc($result);
+                        $uname = $_SESSION['username'];
+                        $query = "SELECT * FROM user_data WHERE username= '$uname'";
+                        $result = $conn->query($query);
+                        $rows = mysqli_fetch_assoc($result);
 
-$username = $rows['username'];           
+                        $username = $rows['username'];           
 
-$adminCast = "admin";
+                        $adminCast = "admin";
 
-if($_SESSION['cast']!==$adminCast){
-    ?> 
-    <script>
-        alert("권한이 없습니다.");
-        location.replace("<?php echo $URL?>");
-    </script>
-    <?php   
-}
+                        if($_SESSION['cast']!==$adminCast){
+                            ?> 
+                            <script>
+                                alert("권한이 없습니다.");
+                                location.replace("<?php echo $URL?>");
+                            </script>
+                            <?php   
+                        }
 
-session_start();
+                        session_start();
 
 
 
-if(!isset($_SESSION['username'])) {
-    ?>              
-    <script>
-        alert("권한이 없습니다.");
-        location.replace("<?php echo $URL?>");
-    </script>
-    <?php   
-} else if($_SESSION['cast']==$adminCast) {
-    //cast: admin인 경우
-    ?>
-    <form class="createForm" action="admin_create_cont_action.php" method="POST" enctype="multipart/form-data">
-        <p >
-            <div class="createInput">
-                <label class="createGrid1">아이디: </label><?=$username?>
-                <input class="createGrid2"  type="hidden" name="username" value="<?=$username?>" required />       
-            </div>
-        </p>
-
-        <p>
-            <div class="createInput">
-                <label class="createGrid1">제목</label>
-                <input class="createGrid2" name="ko_title" placeholder="제목" required />
-            </div>
-        </p>
-        <p>
-            <div class="createInput">
-                <label class="createGrid1">Title</label>
-                <input class="createGrid2" name="en_title" placeholder="title" required />
-            </div>
-        </p>
-        <p>
-            <div class="createInput">
-                <label class="createGrid1">내용</label>
-                <div class="admin_editor">
-                    <textarea name="ir1_ko" id="ir1_ko" ></textarea>
-                </div>
-            </div>
-        </p>
-        <p>
-            <div class="createInput">
-                <label class="createGrid1">Content</label>
-                <div class="admin_editor">
-                    <textarea name="ir1_en" id="ir1_en" ></textarea>
-                </div>
-            </div>
-        </p>
-        <p>
-            <input type="submit" onclick="submitContents(this);">
-            <button name="cancel"><a href = "javascript:history.back()">취소</a></button>
-        </p>
-    </form>
-    <?php
-} else {
-    ?>
-    <script>
-        alert("권한이 없습니다.");
-        location.replace("<?php echo $URL?>");
-    </script>                 
-    <?php
-}
-                
-        ?>
+                        if(!isset($_SESSION['username'])) {
+                            ?>              
+                            <script>
+                                alert("권한이 없습니다.");
+                                location.replace("<?php echo $URL?>");
+                            </script>
+                            <?php   
+                        } else if($_SESSION['cast']==$adminCast) {
+                            //cast: admin인 경우
+                            ?>
+                            <form class="createForm" action="admin_create_intro_action.php" method="POST" enctype="multipart/form-data">
+                                <p >
+                                    <div class="createInput">
+                                        <input class="createGrid2"  type="hidden" name="username" value="<?=$username?>" required />       
+                                    </div>
+                                </p>
+                                <p>
+                                    <div class="createInput">
+                                        <label class="createGrid1">내용</label>
+                                        <div class="admin_editor">
+                                            <textarea name="ir1_ko" id="ir1_ko" ></textarea>
+                                        </div>
+                                    </div>
+                                </p>
+                                <p>
+                                    <div class="createInput">
+                                        <label class="createGrid1">Content</label>
+                                        <div class="admin_editor">
+                                            <textarea name="ir1_en" id="ir1_en" ></textarea>
+                                        </div>
+                                    </div>
+                                </p>
+                                <p>
+                                    <input type="submit" onclick="submitContents(this);">
+                                    <button name="cancel"><a href = "javascript:history.back()">취소</a></button>
+                                </p>
+                            </form>
+                            <?php
+                        } else {
+                            ?>
+                            <script>
+                                alert("권한이 없습니다.");
+                                location.replace("<?php echo $URL?>");
+                            </script>                 
+                            <?php
+                        }               
+                        ?>
                     </div>
                 </div>
             </div>
@@ -166,7 +151,7 @@ if(!isset($_SESSION['username'])) {
     function submitContents(elClickedObj) {
      // 에디터의 내용이 textarea에 적용된다.
      oEditors.getById["ir1_ko"].exec("UPDATE_CONTENTS_FIELD", []);
-     oEditors.getById["ir1_en"].exec("UPDATE_CONTENTS_FIELD", []);
+     oEditors2.getById["ir1_en"].exec("UPDATE_CONTENTS_FIELD", []);
 
      // 에디터의 내용에 대한 값 검증은 이곳에서
      // document.getElementById("ir1").value를 이용해서 처리한다.
--- admin_create_intro_action.php
+++ admin_create_intro_action.php
@@ -1,81 +1,28 @@
 <?php
 
-include 'bbdd_db_conn.php';
+include 'cdd_db_conn.php';
 
 
-
-$author = $_POST['author'];
-$author = mysqli_real_escape_string($conn, $author);
 
 $username = $_POST['username'];
 $username = mysqli_real_escape_string($conn, $username);
 
-$category = $_POST['category'];
-$category = mysqli_real_escape_string($conn, $category);
 
-$sess = $_POST['sess'];
-$sess = mysqli_real_escape_string($conn, $sess);
+$ko_cont = $_POST['ir1_ko'];
+$ko_cont = mysqli_real_escape_string($conn, $ko_cont);
 
-$zin = $_POST['zin'];
-$zin = mysqli_real_escape_string($conn, $zin);
-
-$title = $_POST['title'];
-$title = mysqli_real_escape_string($conn, $title);
-
-$content = $_POST['ir1'];
-$content = mysqli_real_escape_string($conn, $content);
-
-$display = $_POST['display'];
-$display = mysqli_real_escape_string($conn, $display);
-
-$memo = $_POST['memo'];
-$memo = mysqli_real_escape_string($conn, $memo);
+$en_cont = $_POST['ir1_en'];
+$en_cont = mysqli_real_escape_string($conn, $en_cont);
 
 
-// $id = "id";
-$sqlNo = "SELECT `no` FROM contents ORDER BY id DESC LIMIT 1";
-// $stmt = mysqli_stmt_init($conn);
-// if (!mysqli_stmt_prepare($stmt, $sqlNo)) {
-//         echo "sqlNo error";
-//     } else {
-//             // mysqli_stmt_bind_param($stmt, , );
-//             mysqli_stmt_execute($stmt);
-//             $resultNo = mysqli_stmt_get_result($stmt);
-//             if($resultNo->num_rows > 0) {
-//                     $rowNo = mysqli_fetch_assoc($resultNo);
-//                     $no = $rowNo['no'];
-//                     $no = intval(intval($no) + 1);
-//                 } else {
-//                         $no = "1";
-//                     }
-//                     // mysqli_stmt_close();
-//                 }
-                
-                // $sqlNo = "SELECT `no` FROM contents ORDER BY id DESC LIMIT 1";
-                $resultNo = $conn->query($sqlNo) or die($conn->error);
-                if($resultNo->num_rows > 0) {
-                    $rowNo = mysqli_fetch_assoc($resultNo);
-                    $no = $rowNo['no'];
-                    $no = intval(intval($no) + 1);
-                } else {
-                    $no = "1";
-                }
-                
                 
                 $sql = "
-                INSERT INTO contents
-                        (no, author, username, category, sess, zin, title, content, display, memo, created)
+                INSERT INTO intro
+                        (username, ko_cont, en_cont, created)
                     VALUES(
-                            '{$no}',
-                            '{$author}',
                             '{$username}',
-                            '{$category}',
-                            '{$sess}',
-                            '{$zin}',
-                            '{$title}',
-                            '{$content}',
-                            '{$display}',
-                            '{$memo}',
+                            '{$ko_cont}',
+                            '{$en_cont}',
                             NOW()
                             )";
                             
@@ -85,7 +32,7 @@ $sqlNo = "SELECT `no` FROM contents ORDER BY id DESC LIMIT 1";
                                 error_log(mysqli_error($conn));
                             }
                             else{
-                                echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
+                                echo("<script>alert('천두동 소개가 작성되었습니다.');location.href='admin_contList.php';</script>");
                             }
                     
                     
