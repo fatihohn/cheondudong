86720ed1809dcb0eeaa3a55f0f701b53ff23327f
fatihohn <fatihohn@gmail.com>
2020-06-23 18:57:20 +0900

    place list

--- place_list.php
+++ place_list.php
@@ -14,10 +14,10 @@ $en_title =  mysqli_real_escape_string($conn, $rowPlace['en_title']);
 if($resultPlace->num_rows > 0) {
     while($rowPlace = $resultPlace->fetch_assoc()) {
         ?>
-        <li id="<?=$place_id?>" name="<?=$place_id?>" class="ko">
+        <li class="ko">
             <?php echo $ko_title; ?>
         </li>
-        <li id="<?=$place_id?>" name="<?=$place_id?>" class="en">
+        <li class="en">
             <?php echo $en_title; ?>
         </li>
 <?php
