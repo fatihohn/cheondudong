39ef92e336926fdad3656e5c533031765d985634
fatihohn <fatihohn@gmail.com>
2020-06-23 19:09:21 +0900

    place list prepare

--- place_list.php
+++ place_list.php
@@ -3,16 +3,30 @@ include "cdd_db_conn.php";
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+// $sqlPlace = "SELECT * FROM places ORDER BY ko_title DESC";
+// $resultPlace = $conn->query($sqlPlace) or die($conn->error);
+
+
 $sqlPlace = "SELECT * FROM places ORDER BY ko_title DESC";
-$resultPlace = $conn->query($sqlPlace) or die($conn->error);
-// $rowPlace = $resultPlace->fetch_assoc();
+// $resultPlace = $conn->query($sqlPlace) or die($conn->error);
+
+$stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sqlPlace)) {
+            // echo "sqlPlace error";
+    } else {
+            // mysqli_stmt_bind_param($stmt, "s", $author);
+            mysqli_stmt_execute($stmt);
+            $resultPlace = mysqli_stmt_get_result($stmt);
+    }
+
+
 
 
 if($resultPlace->num_rows > 0) {
     while($rowPlace = $resultPlace->fetch_assoc()) {
         $place_id =  $rowPlace['id'];
-        $ko_title =  mysqli_real_escape_string($conn, $rowPlace['ko_title']);
-        $en_title =  mysqli_real_escape_string($conn, $rowPlace['en_title']);
+        $ko_title =  $rowPlace['ko_title'];
+        $en_title =  $rowPlace['en_title'];
         echo "
         <li class='ko'>
             <div class='place_title'>";
