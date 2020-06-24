a92303be9314b8ef7b70d2dde6ecea09390042ad
fatihohn <fatihohn@gmail.com>
2020-06-24 15:16:00 +0900

    detail layout

--- detail.php
+++ detail.php
@@ -21,65 +21,103 @@ $detailRef_en = "reference list";
 ?>
 
 
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
 
-<div id="detail_wrap">
-    <div id="detail_box">
-        <div id="detail_box_area">
-            <div id="detail_marker">
-                <img src="<?php echo $detailMarker;?>" alt="<?php echo $detailTitle;?>">
-            </div>
-            <div id="detail_title">
-                <h3 class="ddobag ko">
-                    <?php echo $detailTitle;?>
-                </h3>
-                <h3 class="ddobag en">
-                    <?php echo $detailTitle_en;?>
-                </h3>
-            </div>
-            <div id="detail_point">
+<?php include "head.php"; ?>
+
+
+    </head>
+    <body>
+
+        <header>
+        <!-- header: 헤더, 메뉴바 역할. 메뉴 버튼 클릭하면 menu화면 보여줌. 제목 클릭하면 map으로 넘어감-->
+            <?php include "header.php"; ?>
+        </header>
+        
+        
+            
+        <section>
                 
-                <p class="detail_address ko">
-                    <?php echo $detailAddress;?>
-                </p>
-                <p class="detail_address en">
-                    <?php echo $detailAddress_en;?>
-                </p>
-                <p class="detail_coord ko">
-                    <?php echo $detailCoord;?>
-                </p>
-                <p class="detail_coord en">
-                    <?php echo $detailCoord;?>
-                </p>
-            </div>
-            <div id="detail_cont">
-                <div class="ko">
-                    <?php echo $detailCont;?>
+            <!-- front page: 전체화면, <천두동 동두천> 제목, 클릭하면 map으로 넘어감 -->
+            <?php include "front.php"; ?>
+            <!-- menu: 메뉴 내비 바 -->
+            <?php include "menu.php"; ?>
+
+
+            <div id="detail_wrap">
+                <div id="detail_box">
+                    <div id="detail_box_area">
+                        <div id="detail_marker">
+                            <img src="<?php echo $detailMarker;?>" alt="<?php echo $detailTitle;?>">
+                        </div>
+                        <div id="detail_title">
+                            <h3 class="ddobag ko">
+                                <?php echo $detailTitle;?>
+                            </h3>
+                            <h3 class="ddobag en">
+                                <?php echo $detailTitle_en;?>
+                            </h3>
+                        </div>
+                        <div id="detail_point">
+                            
+                            <p class="detail_address ko">
+                                <?php echo $detailAddress;?>
+                            </p>
+                            <p class="detail_address en">
+                                <?php echo $detailAddress_en;?>
+                            </p>
+                            <p class="detail_coord ko">
+                                <?php echo $detailCoord;?>
+                            </p>
+                            <p class="detail_coord en">
+                                <?php echo $detailCoord;?>
+                            </p>
+                        </div>
+                        <div id="detail_cont">
+                            <div class="ko">
+                                <?php echo $detailCont;?>
+                            </div>
+                            <div class="en">
+                                <?php echo $detailCont_en;?>
+                            </div>
+                        </div>
+                        <div id="detail_img">
+                            <?php echo $detailImg;?>
+                        </div>
+                        <div id="detail_work">
+                            <p class="work_title ko ddobag">
+                                관련 작품
+                            </p>
+                            <p class="work_title en ddobag">
+                                Works
+                            </p>
+                            <?php echo $detailWork;?>
+                        </div>
+                        <div id="detail_ref">
+                            <p class="ref_title ko ddobag">
+                                참고 자료
+                            </p>
+                            <p class="ref_title en ddobag">
+                                References
+                            </p>
+                            <?php echo $detailRef;?>
+                        </div>
+                    </div>
                 </div>
-                <div class="en">
-                    <?php echo $detailCont_en;?>
-                </div>
             </div>
-            <div id="detail_img">
-                <?php echo $detailImg;?>
-            </div>
-            <div id="detail_work">
-                <p class="work_title ko ddobag">
-                    관련 작품
-                </p>
-                <p class="work_title en ddobag">
-                    Works
-                </p>
-                <?php echo $detailWork;?>
-            </div>
-            <div id="detail_ref">
-                <p class="ref_title ko ddobag">
-                    참고 자료
-                </p>
-                <p class="ref_title en ddobag">
-                    References
-                </p>
-                <?php echo $detailRef;?>
-            </div>
-        </div>
-    </div>
-</div>
+
+            <?php include "map.php"; ?>
+
+        </section>
+
+        <footer>
+        <!-- footer: detail, menu 화면 아래에서 저작권 정보 보여줌. -->
+            <?php include "footer.php"; ?>
+        </footer>
+        
+        
+        <?php include "jsGroup.php"; ?>
+    </body>
+</html>
