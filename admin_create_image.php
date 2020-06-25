391f70fbbb3f4fcf7c5d12b1e35bec70eaddd2c2
fatihohn <fatihohn@gmail.com>
2020-06-25 23:02:02 +0900

    iframe size

--- admin_create_image.php
+++ admin_create_image.php
@@ -30,7 +30,7 @@ $URL = "./admin_create_image.php";
 <div id="create_image_wrap">
     <div id="attached_image_list"></div>
     <div id="create_image">
-        <iframe  class="attach_frame" src="admin_attach_image.php"></iframe>
+        <iframe  class="attach_frame" src="admin_attach_image.php" style="width:596px; height:240px;"></iframe>
     </div>
     <div>
     <button id="img_attach">이미지 확인</button>
