24bd6eafcd0fe33859a40aba59b8aeccd9e0f786
fatihohn <fatihohn@gmail.com>
2020-06-25 22:27:06 +0900

    js attach

--- admin_create_image.php
+++ admin_create_image.php
@@ -38,6 +38,7 @@ $URL = "./admin_create_image.php";
 </div>
 
 <script src="static/js/admin_attach.js"></script>
+<script src="static/js/admin_jsGroup.js"></script>
 <!-- <script>
     function tableImgSize() {
         let tableImgAll = document.querySelectorAll(".table_img");
