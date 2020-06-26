5655deed231ece130916a10275771061e48eb202
fatihohn <fatihohn@gmail.com>
2020-06-27 01:34:31 +0900

    add img layout

--- admin_attach_image.php
+++ admin_attach_image.php
@@ -65,19 +65,21 @@
                 </p>
                 <p>
                     <div class="createInput">
-                    <label class="createGrid1">제목</label>
-                    <input class="createGrid2" name="ko_title" placeholder="제목" required />
-                        <input class="createGrid2" name="en_title" placeholder="Title" required />
+                        <label class="createGrid1">제목</label>
+                        <div>
+                            <input class="createGrid2" name="ko_title" placeholder="제목" required />
+                            <input class="createGrid2" name="en_title" placeholder="Title" required />
+                        </div>
                     </div>
                 </p>
               
                 <p>
                     <div class="createInput">
                         <label class="createGrid1">내용</label>
-                        <!-- <div class="admin_editor"> -->
+                        <div class="admin_editor">
                             <textarea name="ko_cont" id="ko_cont" placeholder="내용" ></textarea>
                             <textarea name="en_cont" id="en_cont" placeholder="Content"></textarea>
-                        <!-- </div> -->
+                        </div>
                     </div>
                 </p>
                 <!-- <p>
