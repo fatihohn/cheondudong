5a6f71f05cd276608616b1f44819cf04ec50bc90
fatihohn <fatihohn@gmail.com>
2020-06-27 14:30:26 +0900

    attach link

---
+++ admin_attach_ref.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<?php include 'admin_head.php'; ?>
+
+</head>
+
+<body>
+ 
+        
+ 
+<section>
+    
+    <div id="create_ref_wrap">
+        <center>
+            <h3>참고자료 추가</h3>
+        </center>
+        <?php    
+        include 'cdd_db_conn.php';   
+        $URL = "./admin_attach_ref.php";
+
+        $uname = $_SESSION['username'];
+        $query = "SELECT * FROM user_data WHERE username= '$uname'";
+        $result = $conn->query($query);
+        $rows = mysqli_fetch_assoc($result);
+
+        $username = $rows['username'];           
+
+        $adminCast = "admin";
+
+        //현재 작성중 장소 id = new_id
+        $sqlLatestPlace = "SELECT id FROM places ORDER BY id DESC LIMIT 1";
+        $resultLatestPlace = $conn->query($sqlLatestPlace);
+        $rowLatestPlace = mysqli_fetch_assoc($resultLatestPlace);
+        $latest_id = $rowLatestPlace['id'];
+        $new_id = intval(intval($latest_id) + 1);
+        $place_id = $new_id;
+
+        session_start();
+
+
+
+        if(!isset($_SESSION['username'])) {
+            ?>              
+            <script>
+                alert("권한이 없습니다.");
+                location.replace("<?php echo $URL?>");
+            </script>
+            <?php   
+        } else if($_SESSION['cast']==$adminCast) {
+            //cast: admin인 경우
+            ?>
+            <form class="createForm" action="admin_attach_ref_action.php" method="POST" enctype="multipart/form-data">
+                <p>
+                    <div class="createInput">
+                        <input class="createGrid2"  type="hidden" name="place_id" value="<?=$place_id?>" required />       
+                    </div>
+                </p>
+                <p>
+                    <div class="createInput">
+                        <label class="createGrid1">제목</label>
+                        <div>
+                            <input class="createGrid2" name="ko_title" placeholder="제목" required />
+                            <input class="createGrid2" name="en_title" placeholder="Title" required />
+                        </div>
+                    </div>
+                </p>
+                <p>
+                    <div class="createInput">
+                        <label class="createGrid1">링크</label>
+                        <div class="admin_editor">
+                            <input type="url" name="ref_link" id="ref_link" />
+                        </div>
+                    </div>
+                </p>
+                <p>
+                    <!-- <input type="submit" onclick="submitContents(this);"> -->
+                    <input id="attach_ref" type="submit" >
+                    <button name="cancel"><a href = "javascript:history.back()">취소</a></button>
+                </p>
+            </form>
+            <?php
+        } else {
+            ?>
+            <script>
+                alert("권한이 없습니다.");
+                location.replace("<?php echo $URL?>");
+            </script>                 
+            <?php
+        }               
+        ?>
+        
+    </div>
+        
+</section>
+
+</body>
+
+</html>
--- admin_create_ref.php
+++ admin_create_ref.php
@@ -1,8 +1,58 @@
-참고자료 목록->ajax
+<!-- 참고자료 목록->ajax
     참고자료 목록 불러오기
 
 참고자료 추가 버튼->ajax
     파일 인풋
     저장 버튼->ajax
-        참고자료 목록에 추가
+        참고자료 목록에 추가 -->
+
+        <!-- 관련작품 목록->ajax
+    관련작품 목록 불러오기
+
+관련작품 추가 버튼->ajax
+    파일 인풋
+    저장 버튼->ajax
+        관련작품 목록에 추가 -->
     
+
+    <!-- 이미지 목록->ajax
+    이미지 목록 불러오기
+
+이미지 추가 버튼->ajax
+    파일 인풋
+    저장 버튼->ajax
+        이미지 목록에 추가 -->
+    
+
+<?php 
+include 'cdd_db_conn.php';   
+// $URL = "./admin_create_ref.php";
+
+// //현재 작성중 장소 id = new_id
+// $sqlLatestPlace = "SELECT id FROM places ORDER BY id DESC LIMIT 1";
+// $resultLatestPlace = $conn->query($sqlLatestPlace);
+// $rowLatestPlace = mysqli_fetch_assoc($resultLatestPlace);
+// $latest_id = $rowLatestPlace['id'];
+// $new_id = intval(intval($latest_id) + 1);
+
+
+
+
+
+
+?>
+
+
+
+<div id="create_ref_wrap">
+    <div id="create_ref">
+        <iframe  class="attach_frame" src="admin_attach_ref.php" style="width:100%; max-width:596px; height:900px;"></iframe>
+    </div>
+    <div id="ref_attach">
+        참고자료 새로고침
+    </div>
+
+    <div id="attached_ref_list"></div>
+</div>
+
+<script src="static/js/admin_attach.js"></script>
