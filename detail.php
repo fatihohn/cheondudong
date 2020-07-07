31a30a33ac1a9c5716dc7c1e1d05b4158a503cdf
fatihohn <fatihohn@gmail.com>
2020-07-07 18:52:33 +0900

    detail memo empty string

--- detail.php
+++ detail.php
@@ -150,7 +150,7 @@ $stmt = mysqli_stmt_init($conn);
                         </div>
                         <?php
                         $sessLang = $_SESSION['language'];
-                        if(isset($detailMemo) && !isset($detailMemo_en)) {
+                        if(isset($detailMemo) && !isset($detailMemo_en) || $detailMemo_en == "") {
                             if($sessLang == "ko") {
                                 ?>
                                 <div id="detail_memo">
@@ -161,7 +161,7 @@ $stmt = mysqli_stmt_init($conn);
                                 </div>
                                 <?php
                             }
-                        } else if(!isset($detailMemo) && isset($detailMemo_en)) {
+                        } else if(!isset($detailMemo) || $detailMemo == "" && isset($detailMemo_en)) {
                             if($seseLang == "en") {
                                 ?>
                                 <div id="detail_memo">
