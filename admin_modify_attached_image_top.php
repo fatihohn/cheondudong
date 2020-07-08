9831ac236eb14f968c35b697dde92b018aeb0005
fatihohn <fatihohn@gmail.com>
2020-07-08 10:48:11 +0900

    img to top

--- admin_modify_attached_image_top.php
+++ admin_modify_attached_image_top.php
@@ -8,6 +8,10 @@ $resultPlaceId = mysqli_query($conn, $sqlPlaceId);
 $rowPlaceId = $resultPlaceId->fetch_assoc();
 $placeId = $rowPlaceId['place_id'];
         
+$sqlPlaceRealId = "SELECT id FROM places WHERE id = $placeId";
+$resultPlaceRealId = mysqli_query($conn, $sqlPlaceRealId);
+$rowPlaceRealId = $resultPlaceRealId->fetch_assoc();
+$placeRealId = $rowPlaceRealId['id'];
         
         $sql = 
         "UPDATE images SET 
@@ -18,7 +22,7 @@ $placeId = $rowPlaceId['place_id'];
 $result = mysqli_query($conn, $sql);
 
 if($result){
-                echo("<script>alert('이미지 순서가 수정되었습니다.');location.href='admin_detail.php?id='+".$placeId.";</script>");
+                echo("<script>alert('이미지 순서가 수정되었습니다.');location.href='admin_detail.php?id='+".$placeRealId.";</script>");
             } else {
                 echo '이미지 저장실패. 관리자에게 문의해주세요';
                 error_log(mysqli_error($conn));
