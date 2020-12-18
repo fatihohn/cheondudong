d9a90e3588c6f8f66a7e2a26b93099293f0a8133
fatihohn <fatihohn@gmail.com>
2020-12-18 22:31:43 +0900

    lettermix

--- index.php
+++ index.php
@@ -6,60 +6,80 @@
 
 
     </head>
-    <body >
-        <div id="index_body">
-
-            <div id="index_bg_line">
-                <header>
-                <!-- header: 헤더, 메뉴바 역할. 메뉴 버튼 클릭하면 menu화면 보여줌. 제목 클릭하면 map으로 넘어감-->
-                    <?php include "header.php"; ?>
-                </header>    
-                <section>
-                    <div id="front_wrap">
-                        <div id="front_box">
-                            <div id="front_title">
-                                <form method="post" action="language_session.php">
-                                    <!-- <input class="front_img" type="image" src="static/img/front.png" alt="Submit" /> -->
-                                    <div class="lang_select">
-                                        <input class='lang_btn' type='radio' id='ko_btn'name='language' value='ko' checked>
-                                        <label for='ko_btn'>한국어</label>
-                                        <input class='lang_btn' type='radio' id='en_btn'name='language' value='en'>
-                                        <label for='en_btn'>English</label>
-                                    </div>
-                                    <div class="front_msg">
-                                        <div class="front_msg_letter cheon"></div>
-                                        <div class="front_msg_letter du"></div>
-                                        <div class="front_msg_letter dong"></div>
-                                    </div>
-    
-                                </form>
-        
-        
-                            </div>
-                            <!-- <div id="lang_select" class="front_lang">
-                                <div id="ko" class="tooling lang_selector">
-                                    한국어
+    <body id="index_body">
+        <div id="index_bg_line">
+            <header>
+            <!-- header: 헤더, 메뉴바 역할. 메뉴 버튼 클릭하면 menu화면 보여줌. 제목 클릭하면 map으로 넘어감-->
+                <?php include "header.php"; ?>
+            </header>    
+            <section>
+                <div id="front_wrap">
+                    <div id="front_box">
+                        <div id="front_title">
+                            <form method="post" action="language_session.php">
+                                <!-- <input class="front_img" type="image" src="static/img/front.png" alt="Submit" /> -->
+                                <div class="lang_select">
+                                    <input class='lang_btn' type='radio' id='ko_btn'name='language' value='ko' checked>
+                                    <label for='ko_btn'>한국어</label>
+                                    <input class='lang_btn' type='radio' id='en_btn'name='language' value='en'>
+                                    <label for='en_btn'>English</label>
                                 </div>
-                                <div id="en" class="tooling lang_selector">
-                                    English
+                                <div class="front_msg">
+                                    <div class="front_msg_letter cheon left"></div>
+                                    <div class="front_msg_letter du center"></div>
+                                    <div class="front_msg_letter dong right"></div>
                                 </div>
-                            </div> -->
+                            </form>
                         </div>
                     </div>
-                </section>
-        
-                <footer>
-                <!-- footer: detail, menu 화면 아래에서 저작권 정보 보여줌. -->
-                    <?php include "footer.php"; ?>
-                </footer>
+                </div>
+            </section>
     
-            </div>
+            <footer>
+            <!-- footer: detail, menu 화면 아래에서 저작권 정보 보여줌. -->
+                <?php include "footer.php"; ?>
+            </footer>
+
         </div>
         
         
     
         <?php include "jsGroup.php"; ?>
 
+        <script>
+            var frontMsg = document.querySelector(".front_msg");
+            var letterCheon = document.querySelector(".front_msg_letter.cheon");
+            var letterDu = document.querySelector(".front_msg_letter.du");
+            var letterDong = document.querySelector(".front_msg_letter.dong");
+            
+            frontMsg.onmouseover = function() {
+                letterMix();
+            }
+
+            setInterval(() => {
+                letterMix();
+            }, 1000);
+
+
+            function letterMix() {
+                if(letterCheon.classlist.contains("left")) {
+                    letterCheon.classList.remove("left");
+                    letterCheon.classList.add("right");
+                } else {
+                    letterCheon.classList.remove("right");
+                    letterCheon.classList.add("left");
+
+                }
+                if(letterDong.classlist.contains("right")) {
+                    letterDong.classList.remove("right");
+                    letterDong.classList.add("left");
+                } else {
+                    letterDong.classList.remove("left");
+                    letterDong.classList.add("right");
+
+                }
+            }
+        </script>
         <script>
             function indexInit() {
                 // let headerWrap = document.getElementById("header_wrap");
