9a208f419a199b2a99f54a266152466d3f0e0ce7
fatihohn <fatihohn@gmail.com>
2020-12-20 19:21:55 +0900

    menu btn

--- jsGroup.php
+++ jsGroup.php
@@ -29,11 +29,12 @@
 
     function manageNav() {
         let menuBtn = document.querySelector(".menu");
-        if(document.querySelector("#menu_wrap")) {
-            let navigation = document.querySelector("#menu_wrap");
-        } else {
-            let navigation = document.querySelector("#menu_wrap");
-        }
+        let navigation = document.querySelector("#menu_wrap");
+        // if(document.querySelector("#menu_wrap")) {
+        //     let navigation = document.querySelector("#menu_wrap");
+        // } else {
+        //     let navigation = document.querySelector("#menu_wrap");
+        // }
         // let overlay = document.getElementById("overlay");
 
         function showNav() {
