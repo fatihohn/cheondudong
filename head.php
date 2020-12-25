2685df438fbd66e9805e6a031581c66823d2b137
fatihohn <fatihohn@gmail.com>
2020-12-25 11:43:00 +0900

    analytics

--- head.php
+++ head.php
@@ -1,35 +1,44 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">	
-    
-    <title>천두동·동두천</title>
-    <link rel="canonical" href="https://cdd-ddc.org" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">	
 
-    <meta name="description" content="천두동 장소들">
+<title>천두동·동두천</title>
+<link rel="canonical" href="https://cdd-ddc.org" />
 
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-    <meta name="keywords" content="동두천 지도, 동두천지도, 천두동지도, 천두동 지도, 지도, 천두동, 동두천, 기지촌, 기지촌 역사, 미군기지, 성병관리소, 낙검자수용소, 상패동, 턱거리, 소요산, 천두동 동두천">
-    
-    <!-- <meta name="robots" content="noindex"> -->
+<meta name="description" content="천두동 장소들">
+
+<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta name="keywords" content="동두천 지도, 동두천지도, 천두동지도, 천두동 지도, 지도, 천두동, 동두천, 기지촌, 기지촌 역사, 미군기지, 성병관리소, 낙검자수용소, 상패동, 턱거리, 소요산, 천두동 동두천">
+
+<!-- <meta name="robots" content="noindex"> -->
 
 
-	<meta property="og:type" content="website">
-	<meta property="og:title" content="천두동-동두천">
-	<meta property="og:description" content="천두동의 장소들">
-	<meta property="og:url" content="https://cdd-ddc.org">
-	<meta property="og:image" content="대표 이미지 제작 후 입력">
+<meta property="og:type" content="website">
+<meta property="og:title" content="천두동-동두천">
+<meta property="og:description" content="천두동의 장소들">
+<meta property="og:url" content="https://cdd-ddc.org">
+<meta property="og:image" content="대표 이미지 제작 후 입력">
 
-  	<link rel="apple-touch-icon" sizes="180x180" href="아이콘 제작 후 입력">
-    <link rel="icon" type="image/png" sizes="32x32" href="아이콘 제작 후 입력">
-    <link rel="icon" type="image/png" sizes="16x16" href="아이콘 제작 후 입력">
+<link rel="apple-touch-icon" sizes="180x180" href="아이콘 제작 후 입력">
+<link rel="icon" type="image/png" sizes="32x32" href="아이콘 제작 후 입력">
+<link rel="icon" type="image/png" sizes="16x16" href="아이콘 제작 후 입력">
 
-    
-    <script src='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.js'></script>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css' rel='stylesheet' />
 
-    <!-- <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.css' rel='stylesheet' /> -->
-    <!-- <link href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v2.3.0/mapbox-gl-geocoder.css' rel='stylesheet' /> -->
+<script src='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css' rel='stylesheet' />
 
-    <link rel="stylesheet" href="static/css/all.css?after" type="text/css">
-    <link rel="stylesheet" href="static/css/header.css?after" type="text/css">
-    <link rel="stylesheet" href="static/css/main.css?after" type="text/css">
+<!-- <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.css' rel='stylesheet' /> -->
+<!-- <link href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v2.3.0/mapbox-gl-geocoder.css' rel='stylesheet' /> -->
 
+<link rel="stylesheet" href="static/css/all.css?after" type="text/css">
+<link rel="stylesheet" href="static/css/header.css?after" type="text/css">
+<link rel="stylesheet" href="static/css/main.css?after" type="text/css">
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MJL1DBK5ZR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MJL1DBK5ZR');
+</script>
